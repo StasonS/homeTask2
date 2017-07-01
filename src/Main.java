cd4c402005867c92fa09a977b1a1bf147694aec6
StasonS <stan.stryzh@gmail.com>
2017-07-01 22:51:28 +0300

    Дал пользователю возможность выбора вырианта вывода на экран

--- src/Main.java
+++ src/Main.java
@@ -24,16 +24,41 @@ public class Main {
     }
 
     static void output(String name, String city, int age, String hobby){
-        System.out.println();
-//        Вариант 1 (табличный):
-        System.out.println("Имя: " + name + "\nГород: " + city + "\nВозоаст: " +
-                "" + age + "\nХобби: " + hobby + "\n");
-//        Вариант 2 (текстовый):
-        System.out.println("Человек по имени " + name + " живет в городе " + city +".\n" +
-                "Этому человеку " + age + " лет и любит он заниматься " + hobby + ".\n");
-//        Вариант 3 (иной):
-        System.out.println(name + " - имя\n" + city + " - город" +
-                "\n" + age + " - возраст\n" + hobby + " - хобби");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Выбери выриант вывода информации:\n1: табличный\n" +
+                "2: текстовый\n3: иной");
+        int i = sc.nextInt();
+        switch (i){
+            case 1:
+                System.out.println("Имя: " + name + "\nГород: " + city + "\nВозоаст: " +
+                        "" + age + "\nХобби: " + hobby + "\n");
+                break;
+            case 2:
+                System.out.println("Человек по имени " + name + " живет в городе " + city +".\n" +
+                        "Этому человеку " + age + " лет и любит он заниматься " + hobby + ".\n");
+                break;
+            case 3:
+                System.out.println(name + " - имя\n" + city + " - город" +
+                        "\n" + age + " - возраст\n" + hobby + " - хобби");
+                break;
+            default:
+                System.out.println("Нет такого варианта");
+                output(name, city, age, hobby);
+                break;
+
+        }
+        
+//        System.out.println();
+////        Вариант 1 (табличный):
+//        System.out.println("Имя: " + name + "\nГород: " + city + "\nВозоаст: " +
+//                "" + age + "\nХобби: " + hobby + "\n");
+////        Вариант 2 (текстовый):
+//        System.out.println("Человек по имени " + name + " живет в городе " + city +".\n" +
+//                "Этому человеку " + age + " лет и любит он заниматься " + hobby + ".\n");
+////        Вариант 3 (иной):
+//        System.out.println(name + " - имя\n" + city + " - город" +
+//                "\n" + age + " - возраст\n" + hobby + " - хобби");
     }
 
     public static void main(String[] args) {
